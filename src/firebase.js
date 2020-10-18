5886013c80eeab1b838b37a397ec0a127ae20597
iradbouzidi <irad.bouzidi@esprit.tn>
2020-10-18 12:31:22 +0100

    deployed

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCOox1YWDxm1Bbm9eoMUKdxkqTgBi9Ue-U",
-    authDomain: "facebook-messenger-clone-irad.firebaseapp.com",
-    databaseURL: "https://facebook-messenger-clone-irad.firebaseio.com",
-    projectId: "facebook-messenger-clone-irad",
-    storageBucket: "facebook-messenger-clone-irad.appspot.com",
-    messagingSenderId: "448504899706",
-    appId: "1:448504899706:web:5c913462ae48035537b3c3",
-    measurementId: "G-1PCWR83P1M"
-})
+  apiKey: "AIzaSyA2TK80CSqbWtJ49xbypYdjyAyYYkZ_g7I",
+  authDomain: "react-messenger-app-d200c.firebaseapp.com",
+  databaseURL: "https://react-messenger-app-d200c.firebaseio.com",
+  projectId: "react-messenger-app-d200c",
+  storageBucket: "react-messenger-app-d200c.appspot.com",
+  messagingSenderId: "327069073096",
+  appId: "1:327069073096:web:3741bfa3b66cb649c9affd",
+  measurementId: "G-KY8XWW4DP8",
+});
 
 const db = firebaseApp.firestore()
 
